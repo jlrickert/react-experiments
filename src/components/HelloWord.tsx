6566b16dae42a5676c0ad3b403bd8bb1f266e132
Jared Rickert <jaredr@ecreative.com>
2021-04-01 21:43:38 -0500

    refactor: basic layout

---
+++ src/components/HelloWord.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export const HelloWorld: React.FC = () => {
+	return <p>Hello World</p>;
+};
---
+++ src/pages/Index/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { HelloWorld } from "components/HelloWord";
+
+export const Index: React.FC<PageProps> = () => {
+	return <HelloWorld />;
+};
